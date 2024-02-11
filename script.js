cdf4bb77b39e9d2c631aacf91d70ae4c1858fd00
aay7ush <aay7ush@gmail.com>
2024-02-11 23:15:38 +0530

    refactor: remove unnecessary code

--- script.js
+++ script.js
@@ -15,28 +15,3 @@ const closeMenu = () => {
 
 menuBtn.addEventListener("click", openMenu)
 closeBtn.addEventListener("click", closeMenu)
-
-// Scroll Animation
-document.lastScrollPosition = 0
-document.lastCentered = 0
-document.onWayTo = null
-
-document.addEventListener("scroll", () => {
-  const sections = [...document.querySelectorAll("section")]
-  const currentScrollPosition = window.pageYOffset + window.innerHeight / 2
-
-  sections.forEach((section, index) => {
-    const sectionTop = section.offsetTop
-    const sectionHeight = section.offsetHeight
-
-    if (
-      currentScrollPosition >= sectionTop &&
-      currentScrollPosition <= sectionTop + sectionHeight
-    ) {
-      section.querySelector(".product__content").classList.add("active")
-      document.lastCentered = index
-    } else {
-      section.querySelector(".product__content").classList.remove("active")
-    }
-  })
-})
